c9a3ea4a41cd90f58ed817bca2981948b6278b84
didosaeed <didosaeed89@gmail.com>
2021-04-24 08:30:04 +0200

    tic tac toe & text to morse code

---
+++ text-to-morse.py
@@ -0,0 +1,20 @@
+def to_morse_code(text):
+
+    code = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+            'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+            'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+            'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+            'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+            'Z': '--..', ' ': '.....'}
+
+    output = ""
+
+    for l in text:
+        output += code[l.upper()]
+    return output
+
+
+text = input("Please input text to convert it to morse code: ")
+
+
+print(to_morse_code(text))
